fc372266b8a6cf28756a5c0a1d56cf48c6252384
Ozzie Harb <ozharb@gmail.com>
2021-01-20 10:06:53 -0800

    list service typo

--- src/lists/lists-router.js
+++ src/lists/lists-router.js
@@ -2,7 +2,7 @@
 const path = require('path')
 const express = require('express');
 const xss = require('xss');
-const ListsService = require('./Lists-service');
+const ListsService = require('./lists-service');
 
 const listsRouter = express.Router();
 const jsonParser = express.json();
