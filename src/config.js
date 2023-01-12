730369e6f57064f29b8f77fd9d14390d78ac5c0b
Ozzie Harb <ozharb@gmail.com>
2023-01-11 19:36:23 -0800

    databse url

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
-    process.env.DATABASE_URL ||
+    "postgres://mzyftcfd:dxNTpUyMK6PCkdiQVmJT4W8f-PCfaACm@hansken.db.elephantsql.com/mzyftcfd" ||
     "postgres://mzyftcfd:dxNTpUyMK6PCkdiQVmJT4W8f-PCfaACm@hansken.db.elephantsql.com/mzyftcfd",
   JWT_SECRET: process.env.JWT_SECRET || "oz-special-jwt-secret",
 };
