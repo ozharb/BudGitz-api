d8442360ad15fd5562e26d8657527df23486abaf
Ozzie Harb <ozharb@gmail.com>
2023-01-11 19:31:44 -0800

    test env

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -1,61 +1,50 @@
+const express = require("express");
+const path = require("path");
+const UsersService = require("./users-service");
+const { requireAuth } = require("../middleware/jwt-auth");
+const usersRouter = express.Router();
+const jsonBodyParser = express.json();
 
-const express = require('express')
-const path = require('path')
-const UsersService = require('./users-service')
-const { requireAuth } = require('../middleware/jwt-auth')
-const usersRouter = express.Router()
-const jsonBodyParser = express.json()
+usersRouter.post("/", jsonBodyParser, (req, res, next) => {
+  const { password, user_name, full_name, nickname } = req.body;
+  console.log("hello");
+  for (const field of ["full_name", "user_name", "password"])
+    if (!req.body[field])
+      return res.status(400).json({
+        error: { message: `Missing '${field}' in request body` },
+      });
 
-usersRouter
- 
+  // TODO: check user_name doesn't start with spaces
 
-  .post('/', jsonBodyParser, (req, res, next) => {
-    const { password, user_name, full_name, nickname } = req.body
+  const passwordError = UsersService.validatePassword(password);
 
-    for (const field of ['full_name', 'user_name', 'password'])
-      if (!req.body[field])
-        return res.status(400).json({
-          error: {"message": `Missing '${field}' in request body`}
-        })
+  if (passwordError) return res.status(400).json({ error: passwordError });
 
-    // TODO: check user_name doesn't start with spaces
+  UsersService.hasUserWithUserName(req.app.get("db"), user_name)
+    .then((hasUserWithUserName) => {
+      if (hasUserWithUserName)
+        return res.status(400).json({ error: `Username already taken` });
 
-    const passwordError = UsersService.validatePassword(password)
+      return UsersService.hashPassword(password).then((hashedPassword) => {
+        const newUser = {
+          user_name,
+          password: hashedPassword,
+          full_name,
+          nickname,
+          date_created: "now()",
+        };
 
-    if (passwordError)
-      return res.status(400).json({ error: passwordError })
+        return UsersService.insertUser(req.app.get("db"), newUser).then(
+          (user) => {
+            res
+              .status(201)
+              .location(path.posix.join(req.originalUrl, `/${user.id}`))
+              .json(UsersService.serializeUser(user));
+          }
+        );
+      });
+    })
+    .catch(next);
+});
 
-    UsersService.hasUserWithUserName(
-      req.app.get('db'),
-      user_name
-    )
-      .then(hasUserWithUserName => {
-        if (hasUserWithUserName)
-          return res.status(400).json({ error: `Username already taken` })
-
-        return UsersService.hashPassword(password)
-          .then(hashedPassword => {
-            const newUser = {
-              user_name,
-              password: hashedPassword,
-              full_name,
-              nickname,
-              date_created: 'now()',
-            }
-
-            return UsersService.insertUser(
-              req.app.get('db'),
-              newUser
-            )
-              .then(user => {
-                res
-                  .status(201)
-                  .location(path.posix.join(req.originalUrl, `/${user.id}`))
-                  .json(UsersService.serializeUser(user))
-              })
-          })
-      })
-      .catch(next)
-  })
-
-module.exports = usersRouter
+module.exports = usersRouter;
