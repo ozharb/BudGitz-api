fa91476202f9c22b635ba628b775b16403daeeb4
Ozzie Harb <ozharb@gmail.com>
2023-01-11 18:34:59 -0800

    require pg

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,10 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
+
+const pg = require("pg");
+pg.defaults.ssl = process.env.NODE_ENV === "production";
 
 module.exports = {
-  "migrationsDirectory": "migrations",
-  "driver": "pg",
-  "connectionString": (process.env.NODE_ENV === 'test')
-     ? process.env.TEST_DATABASE_URL
-     : process.env.DATABASE_URL,
-     ssl: process.env.NODE_ENV === 'production',
-}
+  migrationsDirectory: "migrations",
+  driver: "pg",
+  connectionString:
+    process.env.NODE_ENV === "test"
+      ? process.env.TEST_DATABASE_URL
+      : process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production",
+};
