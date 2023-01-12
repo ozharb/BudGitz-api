4415da98a1aa72e2a6b1c2a17523c082545704b1
Ozzie Harb <ozharb@gmail.com>
2023-01-11 17:44:39 -0800

    updated config file

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = {
-  PORT: process.env.PORT || 10000,
+  PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
     process.env.DATABASE_URL ||
